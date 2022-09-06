263c649845bcbaf785e17984f657b3cefb8ecd1d
Seong Gi Dong <sgd0947@gmail.com>
2022-09-06 18:24:21 +0900

    style(config): eslint 룰 미적용된 사항 추가 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,8 @@ import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 
 export default MyApp;
