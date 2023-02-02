1b6c85a6b204c048e42d22d919ae293dd5085537
Seong Gi Dong <sgd0947@gmail.com>
2023-02-02 16:46:15 +0900

    feat(lighthouse): lighthouse collect url updated

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      url: ['http://localhost:3000'],
+      url: ['http://localhost:3000', 'http://localhost:3000/context'],
       collect: {
         numberOfRuns: 5,
       },
