89526a769444b1209aecfceef6cc620b9eac0f6b
Seong Gi Dong <sgd0947@gmail.com>
2023-02-13 18:27:09 +0900

    chore(lighthouse): 카테고리별 점수 측정 추가

--- lighthouserc.js
+++ lighthouserc.js
@@ -37,6 +37,12 @@ module.exports = {
         'html-has-lang': 'off',
         'robots-txt': 'off',
         viewport: 'off',
+        // performance 카테고리 점수가 70점 미만이면 warning
+        'categories:performance': ['warn', { minScore: 0.7 }],
+        // accessibility 가 60점 미만이면 error
+        'categories:accessibility': ['error', { minScore: 0.5 }],
+        // seo 가 50점 미만이면 error
+        'categories:seo': ['error', { minScore: 0.5 }],
       },
       preset: 'lighthouse:recommended',
     },
